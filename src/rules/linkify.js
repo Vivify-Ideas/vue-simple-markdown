58ccf9e7f3502f03fbbc5a40e45a9d72ed4adfba
Milan Bačkonja <milan.backonja@hotmail.com>
2017-08-31 12:02:49 +0200

    Fix parsing links in lists. Fix parsing links when link contains _

--- src/rules/linkify.js
+++ src/rules/linkify.js
@@ -1,15 +1,15 @@
-const LINK_REGEX = /(^|\s)(http(s)?:\/\/.)(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)(?!\S)/g
+const LINK_REGEX = /(^|\s|>)((?:http(?:s)?:\/\/.)(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b(?:[-a-zA-Z0-9@:%_\+.~#?&//=]*))(\s|$|<)/g
 
 export class Linkify {
   static get RULE_NAME () { return 'linkify' }
 
   static parse (source) {
-    return source.replace(LINK_REGEX, (url, before) => {
+    return source.replace(LINK_REGEX, (all, before, url, after) => {
       url = url.trim()
 
       const href = url.substr(0, 4) !== 'http' ? `http://${url}` : url
 
-      return `${before}<a href="${href}" target="_blank">${url}</a>`
+      return `${before}<a href="${href}" target="_blank">${url}</a>${after}`
     })
   }
 }
--- src/rules/strong.js
+++ src/rules/strong.js
@@ -1,11 +1,11 @@
-const STRONG_REGEX_1 = /(?:__)(.+?)(?:__)/g
-const STRONG_REGEX_2 = /(?:\*\*)(.+?)(?:\*\*)/g
+const STRONG_REGEX_1 = /(^|\s)(?:__)(.+?)(?:__)($|\s)/g
+const STRONG_REGEX_2 = /(^|\s)(?:\*\*)(.+?)(?:\*\*)($|\s)/g
 
 export class Strong {
   static get RULE_NAME () { return 'strong' }
 
   static parse (source) {
-    source = source.replace(STRONG_REGEX_1, '<strong>$1</strong>')
-    return source.replace(STRONG_REGEX_2, '<strong>$1</strong>')
+    source = source.replace(STRONG_REGEX_1, '$1<strong>$2</strong>$3')
+    return source.replace(STRONG_REGEX_2, '$1<strong>$2</strong>$3')
   }
 }
