a4a2a73ade46c72b26162911458cf1bc8a48fe9a
Milan Bačkonja <milan.backonja@hotmail.com>
2018-10-25 16:50:25 +0200

    Improved link and img regex. Switched to yarn

--- src/rules/img.js
+++ src/rules/img.js
@@ -1,6 +1,6 @@
 import { Utils } from './../utils'
 
-const IMG_REGEX = /!\[(.*)\]\(((?:(http[s]?|ftp)?:?\/{0,2})[\w\/\-?#=\.]+)\)/g
+const IMG_REGEX = /!\[(.*)\]\(((?:(http[s]?|ftp)?:?\/{0,2})[\w\/\-+?#=.:]+)\)/g
 
 export class Img {
   static get RULE_NAME () { return 'images' }
