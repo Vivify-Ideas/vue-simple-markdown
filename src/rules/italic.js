a40368c8bf429cf40cfc358a8bdf7ff70d99ad11
markozuzic <markozuzic94@gmail.com>
2019-03-01 11:48:48 +0100

    Change strong regex to allow characters next to the strong tag (#6)
    * Change strong regex to allow characters next to the strong tag

--- src/rules/italic.js
+++ src/rules/italic.js
@@ -1,11 +1,11 @@
-const ITALIC_REGEX_1 = /(^|\s)(?:_)(.+?)(?:_)($|\s)/g
-const ITALIC_REGEX_2 = /(^|\s)(?:\*)(.+?)(?:\*)($|\s)/g
+const ITALIC_REGEX_1 = /(^|\s|"|')(?:_)(.+?)(?:_)/g
+const ITALIC_REGEX_2 = /(^|\s|"|')(?:\*)(.+?)(?:\*)/g
 
 export class Italic {
   static get RULE_NAME () { return 'italic' }
 
   static parse (source) {
-    source = source.replace(ITALIC_REGEX_1, '$1<i>$2</i>$3')
-    return source.replace(ITALIC_REGEX_2, '$1<i>$2</i>$3')
+    source = source.replace(ITALIC_REGEX_1, '$1<i>$2</i>')
+    return source.replace(ITALIC_REGEX_2, '$1<i>$2</i>')
   }
 }
--- src/rules/strong.js
+++ src/rules/strong.js
@@ -1,11 +1,11 @@
-const STRONG_REGEX_1 = /(^|\s)(?:__)(.+?)(?:__)($|\s)/g
-const STRONG_REGEX_2 = /(^|\s)(?:\*\*)(.+?)(?:\*\*)($|\s)/g
+const STRONG_REGEX_1 = /(^|\s|"|')(?:__)(.+?)(?:__)/g
+const STRONG_REGEX_2 = /(^|\s|"|')(?:\*\*)(.+?)(?:\*\*)/g
 
 export class Strong {
   static get RULE_NAME () { return 'strong' }
 
   static parse (source) {
-    source = source.replace(STRONG_REGEX_1, '$1<strong>$2</strong>$3')
-    return source.replace(STRONG_REGEX_2, '$1<strong>$2</strong>$3')
+    source = source.replace(STRONG_REGEX_1, '$1<strong>$2</strong>')
+    return source.replace(STRONG_REGEX_2, '$1<strong>$2</strong>')
   }
 }
