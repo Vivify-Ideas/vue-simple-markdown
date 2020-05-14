3f07262732a298ba2535d00e99fc9e3fd901e02a
Darko Kezic <dkezic@yahoo.com>
2020-05-14 11:04:34 +0200

    Prevent emoji in url (#25)
    * Link regex rule update (#20)
    
    * Updated the link regex rule to allow ; in url
    
    * Link regex rule update (#20)
    
    * Updated the link regex rule to allow ; in url
    
    * To remove emoji : x : from url, replace : with urlencoded version
    
    * Change parameter name
    
    * Remove unused group from regex

--- src/rules/link.js
+++ src/rules/link.js
@@ -1,9 +1,12 @@
-const LINK_REGEX = /\[(.+?)\]\(((?:(http[s]?|ftp):\/{2})?[\w\/\-+?#=.:;!%&]+)\)/g
-
+const LINK_REGEX = /\[(.+?)\]\(((?:(?:http[s]?|ftp):\/{2})?)([\w\/\-+?#=.:;!%&]+)\)/g
 export class Link {
   static get RULE_NAME () { return 'link' }
 
   static parse (source) {
-    return source.replace(LINK_REGEX, '<a href="$2" target="_blank">$1</a>')
+    return source.replace(LINK_REGEX, (match, linkName, urlProtocolDomain, urlPath) => {
+      const url = urlProtocolDomain.trim() + urlPath.trim().replace(/:/g, '%3A')
+
+      return `<a href="${url}" target="_blank">${linkName}</a>`
+    })
   }
 }
--- src/rules/linkify.js
+++ src/rules/linkify.js
@@ -1,12 +1,10 @@
-const LINK_REGEX = /(^|\s|>)((?:http(?:s)?:\/\/.)(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b(?:[-a-zA-Z0-9@:;%_\+.~#?!&//=]*))/g
-
+const LINK_REGEX = /(^|\s|>)((?:http(?:s)?:\/\/.)(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6})\b([-a-zA-Z0-9@:;%_\+.~#?!&//=]*)/g
 export class Linkify {
   static get RULE_NAME () { return 'linkify' }
 
   static parse (source) {
-    return source.replace(LINK_REGEX, (all, before, url) => {
-      url = url.trim()
-
+    return source.replace(LINK_REGEX, (all, before, urlProtocolDomain, urlPath) => {
+      const url = urlProtocolDomain.trim() + urlPath.trim().replace(/:/g, '%3A')
       const href = url.substr(0, 4) !== 'http' ? `http://${url}` : url
 
       return `${before}<a href="${href}" target="_blank">${url}</a>`
