3466e322929608b1d085e8a31b7da4256e998a16
Milan Bačkonja <milan.backonja@hotmail.com>
2021-07-05 12:48:31 +0200

    Code style

--- src/rules/link.js
+++ src/rules/link.js
@@ -1,18 +1,12 @@
-const LINK_REGEX =
-  /\[(.+?)\]\(((?:(?:http[s]?|ftp):\/{2})?)([\w\/\-+?#=.:;!%&]+)\)/g
+const LINK_REGEX = /\[(.+?)\]\(((?:(?:http[s]?|ftp):\/{2})?)([\w\/\-+?#=.:;!%&]+)\)/g
 export class Link {
-  static get RULE_NAME () {
-    return 'link'
-  }
+  static get RULE_NAME () { return 'link' }
 
   static parse (source) {
-    return source.replace(
-      LINK_REGEX,
-      (match, linkName, urlProtocolDomain, urlPath) => {
-        const url =
-          urlProtocolDomain.trim() + urlPath.trim().replace(/:/g, '%3A')
-        return `<a href="${url}" target="_blank">${linkName}</a>`
-      }
-    )
+    return source.replace(LINK_REGEX, (match, linkName, urlProtocolDomain, urlPath) => {
+      const url = urlProtocolDomain.trim() + urlPath.trim().replace(/:/g, '%3A')
+
+      return `<a href="${url}" target="_blank">${linkName}</a>`
+    })
   }
 }
